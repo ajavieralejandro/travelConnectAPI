6e329a6eed1ee2269989023db6f3a76b3f8a7ab7
ajavieralejandro <amorosijavier@gmail.com>
2025-02-23 15:10:28 -0300

    vite configuration update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'https://seashell-app-ytncg.ondigitalocean.app/',  // Tu dominio con HTTPS
+            protocol: 'wss',  // Usar WebSocket seguro
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js','resources/js/app.jsx'],
